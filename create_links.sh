b7b8134d18abeac9af70f9e845386bdc9fc23a76
liangfeng <liang.feng98 AT gmail DOT com>
2014-06-24 19:44:44 +0800

    Put dotfiles in download folder.

--- create_links.sh
+++ create_links.sh
@@ -2,5 +2,28 @@
 
 DOT_FILES="bash_profile bashrc gitconfig tmux.conf"
 for f in $DOT_FILES; do
-    ln -sf ~/.dotfiles/$f ~/.$f
+    ln -sf ~/download/dotfiles/$f ~/.$f
 done
+
+cd ~/download/
+
+if [ -d dircolors-solarized ];
+then
+    cd dircolors-solarized
+    git pull
+    cd ..
+else
+    git clone https://github.com/seebi/dircolors-solarized.git
+fi
+
+ln -sf ~/download/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
+
+if [ -d tmux-colors-solarized ];
+then
+    cd tmux-colors-solarized
+    git pull
+    cd ..
+else
+    git clone https://github.com/seebi/tmux-colors-solarized.git
+fi
+
