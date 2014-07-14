b47d0a4a36bc9cfd2b4c63a3083430e3044dcda6
Liang Feng <liang.feng98@gmail.com>
2014-07-14 18:59:42 +0800

    correct update logic.

--- create_links.sh
+++ create_links.sh
@@ -7,7 +7,15 @@ for f in $DOT_FILES; do
     ln -sf ~/.dotfiles/$f ~/.$f
 done
 
-git submodule init
-git submodule update
+git submodule update --init
+
+cd dircolors-solarized
+git pull origin master
+cd ..
 
 ln -sf ~/.dotfiles/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
+
+cd tmux-colors-solarized
+git pull origin master
+cd ..
+
