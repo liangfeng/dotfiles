0ca5ba1b05330db4e2ab0e73c206e729c8199c2d
Liang Feng <liang.feng98@gmail.com>
2014-07-14 18:51:58 +0800

    use 'git submodule update' instead of 'git pull'

--- create_links.sh
+++ create_links.sh
@@ -7,22 +7,7 @@ for f in $DOT_FILES; do
     ln -sf ~/.dotfiles/$f ~/.$f
 done
 
-if [ -d dircolors-solarized ];
-then
-    cd dircolors-solarized
-    git pull
-    cd ..
-else
-    git clone https://github.com/seebi/dircolors-solarized.git
-fi
+git submodule init
+git submodule update
 
 ln -sf ~/.dotfiles/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
-
-if [ -d tmux-colors-solarized ];
-then
-    cd tmux-colors-solarized
-    git pull
-    cd ..
-else
-    git clone https://github.com/seebi/tmux-colors-solarized.git
-fi
