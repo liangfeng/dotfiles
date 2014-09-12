77e0c038c033a901ae848c482d6f9f986824f197
Liang Feng <liang.feng98@gmail.com>
2014-09-12 10:39:47 -0400

    Use right submodule commands.

--- create_links.sh
+++ create_links.sh
@@ -8,8 +8,14 @@ for f in $DOT_FILES; do
     ln -sf $dotfiles_src_dir/$f ~/.$f
 done
 
-git submodule foreach git pull origin master
+# Update direct-depended submodule.
 git submodule update --init
+
+# Update indirect-depended submodule, if any.
+git submodule foreach git submodule update --init
+
+# Update each checkouted submodules.
+git submodule foreach git pull origin master
 
 ln -sf $dotfiles_src_dir/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
 
